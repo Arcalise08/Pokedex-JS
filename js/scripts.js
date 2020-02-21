bbe7d60d53e7b726f605bd71ac2eda658a9a539e
Arcalise08 <demonata08@gmail.com>
2020-02-21 13:37:22 -0600

    added 3 pokemans

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,14 @@
-alert('Hello world');
-var favoriteFood="pizza";
+/*This is for later 
+/*    squirtle: {type: 'water', height: 0.5},
+    charmander: {type: 'fire', height: 0.6}, */
+var damage = null
+var weak = damage * 2
+var strong = damage / 2
 
-document.write(favoriteFood)
+/*Pokedex Start */
+var repository = [
+    {name:'bulbasaur', type: ['grass' ,'poison'], height: 0.8},
+    {name:'squirtle', type: 'water', height: 0.5},
+    {name:'charmander', type: 'fire', height: 0.7},
+
+]
